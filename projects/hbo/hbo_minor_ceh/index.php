2f0ef449b1dc6334d44616ecba9e371e2e9b446b
Damiaen <damiaen@noardcode.nl>
2021-04-20 09:49:43 +0200

    Added some more no-index checks and removed files.

--- projects/hbo/hbo_minor_ceh/index.php
+++ projects/hbo/hbo_minor_ceh/index.php
@@ -10,7 +10,7 @@ include 'lib/page_enter.php';
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes">
-    <meta name="robots" content="none">
+    <meta name="robots" content="noindex">
     <script src="js/jquery.js"></script>
     <script src="js/main.js"></script>
     <link rel="stylesheet" href="assets/style.css">
